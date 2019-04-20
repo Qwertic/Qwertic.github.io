f7e32e771b738282561e3d2cb710d5c0ca7dd389
Regi Voda <vodaregi@gmail.com>
2019-04-20 22:29:28 +0200

    yet

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'middleman', '~> 4.2'
-gem 'middleman-deploy', '~> 1.0'
 gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-deploy', github: 'middleman-contrib/middleman-deploy', branch: 'master'
+gem 'net-ssh', '~> 5.0', '>= 5.0.2'
+gem 'ptools', '~> 1.3', '>= 1.3.5'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -16,14 +16,10 @@ page '/*.txt', layout: false
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 activate :deploy do |deploy|
-  deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.build_before   = true
+  deploy.deploy_method  = :git
+  deploy.branch         = 'master'
 end
-
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
