61bd90c72d37fe1c0f2f99acbc5afbeef80c88ca
Regi Voda <vodaregi@gmail.com>
2019-04-22 18:41:42 +0200

    navbar fixed, typedJs added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,24 @@
 // This is where it all goes :)
+// This is where it all goes :)
+
+
+const initUpdateNavbarOnScroll = () => {
+  const lavlinks = document.querySelectorAll('.l');
+  const navbar = document.querySelector('.navbar');
+  if (navbar && lavlinks) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-fff');
+        lavlinks.forEach(function(link){
+            link.classList.add('tab-underlined-color');
+});
+      } else {
+        navbar.classList.remove('navbar-fff');
+        lavlinks.forEach(function(link){
+            link.classList.remove('tab-underlined-color');
+});
+      }
+    });
+  }
+}
+initUpdateNavbarOnScroll();
